73678f1a84d07e97e8a30930ae4e4c537ce5a113
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-22 17:53:33 -0300

    styles: atualiza estilização do login de acordo com a pagina home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { FormLogin } from '@/components/FormLogin';
 export default function Index () {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center pt-20 bg-slate-200">
       <FormLogin />
     </main>
   )
--- components/FormLogin.tsx
+++ components/FormLogin.tsx
@@ -21,27 +21,27 @@ const FormLogin = () => {
     }
 
     return (
-        <form className='flex flex-col gap-6 w-1/4 h-auto bg-slate-200 border-purple-700 border-2 shadow-lg p-6 rounded-sm'>
+        <form className='flex flex-col gap-6 md:w-1/4 w-5/6 h-auto bg-slate-800 shadow-lg p-6 rounded-sm'>
             <div className='flex flex-col gap-2'>
                 <div className='flex flex-col'>
-                    <label htmlFor="inputEmail" className='text-purple-700 text-lg font-bold font-mono'>Email:</label>
+                    <label htmlFor="inputEmail" className='text-white text-lg font-bold font-mono'>Email:</label>
                     <input name='inputEmail' type='text'
-                        className='outline-none text-purple-700 p-2 rounded-sm shadow-sm'
+                        className='outline-none bg-slate-50 text-slate-800 p-2 rounded-sm shadow-sm'
                         onChange={(e) => setEmail(e.target.value)}
                         value={email}
                     />
                 </div>
                 <div className='flex flex-col'>
-                    <label htmlFor="inputSenha" className='text-purple-700 text-lg font-bold font-mono'>Senha:</label>
+                    <label htmlFor="inputSenha" className='text-white text-lg font-bold font-mono'>Senha:</label>
                     <input name='inputSenha' type='password'
-                        className='outline-none text-purple-700 p-2 rounded-sm shadow-sm'
+                        className='outline-none bg-slate-50 text-slate-800 p-2 rounded-sm shadow-sm'
                         onChange={(e) => setSenha(e.target.value)}
                         value={senha}
                     />
                 </div>
             </div>
 
-            <button className='bg-purple-700 outline-none rounded-sm p-2'
+            <button className='bg-blue-600 outline-none rounded-sm p-2'
                 onClick={e => tryLogin(e)}
             >
                 <span className='text-lg text-white font-mono font-bold'>Entrar</span>
