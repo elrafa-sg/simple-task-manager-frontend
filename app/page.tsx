bd226c97b332db266353075b18669c30ae978bfa
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-27 23:17:37 -0300

    refactor: melhora tratamento de erros no login

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,45 @@
-import { FormLogin } from '@/components/FormLogin';
+'use client'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { FormLogin } from '@/components/FormLogin'
+import { Toast, ToastType } from '@/components/Toast'
+import { UsuarioService } from '@/services/UsuarioService'
+
+interface ToastState {
+  type: ToastType,
+  open: boolean,
+  message: string,
+  timeout: number
+}
 
 export default function Index () {
+  const [toastState, setToastState] = useState<ToastState>({ type: ToastType.success, open: false, message: '', timeout: 3500 })
+  const router = useRouter()
+
+  async function tryLogin (email: string, senha: string) {
+    const apiResponse = await UsuarioService.login(email, senha)
+    if (apiResponse.status == 200) {
+      router.push('/home')
+    } else {
+      setToastState({
+        open: true,
+        message: apiResponse?.data.message,
+        type: ToastType.danger, timeout: 3500
+      })
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center pt-20 bg-slate-200">
-      <FormLogin />
+      {toastState.open && (
+        <Toast message={toastState.message} type={toastState.type}
+          closeFunction={() => setToastState({ ...toastState, open: false })}
+          timeout={toastState.timeout}
+        />
+      )}
+
+      <FormLogin loginFunction={tryLogin} />
     </main>
   )
 }
--- services/UsuarioService.ts
+++ services/UsuarioService.ts
@@ -4,8 +4,13 @@ const BASE_PATH = '/usuario'
 
 class UsuarioService {
     static async login (email: string, senha: string) {
-        const loginResponse = await apiClient.post(`${BASE_PATH}/login`, { email, senha })
-        return loginResponse
+        return apiClient.post(`${BASE_PATH}/login`, { email, senha })
+            .then(apiResponse => {
+                return apiResponse
+            })
+            .catch(apiErrorResponse => {
+                return apiErrorResponse.response
+            })
     }
 }
 
