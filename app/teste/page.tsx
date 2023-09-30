38dcb38aa3a775cfa2b2d216f5e82601ba780fd5
Rafael Gomes <silveiragomesr@gmail.com>
2023-09-30 00:03:06 -0300

    fix: remove ssr da pagina de teste

--- app/teste/page.tsx
+++ app/teste/page.tsx
@@ -1,26 +1,35 @@
 'use client'
 import { GoogleAuth } from '@/utils/googleAuth'
 
-export default function Index () {
+import dynamic from 'next/dynamic'
+
+const TestePage = () => {
     const googleAuth = new GoogleAuth()
+
+    function handleAuth () {
+        googleAuth.askPermission()
+            .then(permissionResponse => {
+                // enviar para salvar no banco
+                console.log("calendar token ", permissionResponse.access_token)
+            })
+            .catch(permissionErrorResponse => {
+                console.log("permissionErrorResponse ", permissionErrorResponse)
+                // informar ao usuario
+            })
+    }
+
 
     return (
         <main className="flex min-h-screen flex-col items-center pt-20 bg-slate-200">
 
-            <button onClick={async () => {
-                googleAuth.askPermission()
-                    .then(permissionResponse => {
-                        // enviar para salvar no banco
-                        console.log("calendar token ", permissionResponse.access_token)
-                    })
-                    .catch(permissionErrorResponse => {
-                        console.log("permissionErrorResponse ", permissionErrorResponse)
-                        // informar ao usuario
-                    })
-            }}>
+            <button onClick={() => handleAuth()}>
                 GOOGLE AUTH
             </button>
 
         </main>
     )
 }
+
+const Index = dynamic(() => Promise.resolve(TestePage), { ssr: false })
+
+export default Index
