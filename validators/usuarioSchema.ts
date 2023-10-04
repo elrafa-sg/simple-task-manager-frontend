aa8e793fe9b13b9d7048333dc1447762ccf141b2
Rafael Gomes <silveiragomesr@gmail.com>
2023-10-04 14:38:11 -0300

    feat: melhora validação do form de usuário

--- validators/usuarioSchema.ts
+++ validators/usuarioSchema.ts
@@ -1,9 +1,14 @@
 import { z } from 'zod'
 
 const usuarioSchema = z.object({
-    nome: z.string().min(2).max(50),
-    email: z.string().email(),
-    senha: z.string().min(8),
+    nome: z.string()
+        .min(3, { message: 'O nome deve ter no mínimo 3 caracteres' })
+        .max(50, { message: 'O nome deve ter no máximo 50 caracteres' }),
+    email: z.string()
+        .email({ message: 'Email inválido.' }),
+    senha: z.string()
+        .min(8, { message: 'A senha deve ter no mínimo 8 caracteres.' })
+        .max(32, { message: 'A senha deve ter no máximo 32 caracteres.' }),
 })
 
 export default usuarioSchema;
