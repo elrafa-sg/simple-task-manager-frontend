bd2bfeda7fbb748b18640abf2d3253fe9a0dc302
Rafael Gomes <silveiragomesr@gmail.com>
2023-10-13 22:53:05 -0300

    feat: adiciona fluxo de cadastro

--- services/UsuarioService.ts
+++ services/UsuarioService.ts
@@ -12,6 +12,16 @@ class UsuarioService {
                 return apiErrorResponse.response
             })
     }
+
+    static async signup (nome: string, email: string, senha: string) {
+        return apiClient.post(`${BASE_PATH}/signup`, { nome, email, senha })
+            .then(apiResponse => {
+                return apiResponse
+            })
+            .catch(apiErrorResponse => {
+                return apiErrorResponse.response
+            })
+    }
 }
 
 export { UsuarioService }
